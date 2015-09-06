128a24af28e3e52b262afa563f70363ed4a47f06
Dylan Craggs <dylancraggs@Dylans-iMac.local>
2015-09-06 20:10:54 +1000

    Started rough design & development of the forums page

---
+++ Peni/Peni/ForumThread.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Peni
+{
+	public class ForumThread
+	{
+		public string TopicFavorite { get; set; }
+		public string TopicName { get; set; }
+		public string TopicComments { get; set; }
+		public string TopicAuthor { get; set; }
+		public string TopicCreationDate { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Peni.ForumThread"/> class.
+		/// </summary>
+		/// <param name="TopicName">The name of a thread.</param>
+		/// <param name="TopicComments">Number of comments on a thread.</param>
+		/// <param name="TopicAuthor">The author of a thread.</param>
+		/// <param name="TopicCreationDate">The creation date of a thread.</param>
+		public ForumThread (string TopicName, int TopicComments, string TopicAuthor, string TopicCreationDate)
+		{
+			// This obviously needs to be replaced with the proper image
+			this.TopicFavorite = "https://upload.wikimedia.org/wikipedia/en/archive/f/f4/20090120035706!Free_Blue_Star.jpg";
+			this.TopicName = TopicName;
+			this.TopicComments = TopicComments + " Comments";
+			this.TopicAuthor = TopicAuthor;
+			this.TopicCreationDate = TopicCreationDate;
+		}
+	}
+}
+
---
+++ Peni/Peni/Forums.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+
+namespace Peni
+{
+	public partial class Forums : ContentPage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Peni.Forums"/> class.
+		/// </summary>
+		public Forums ()
+		{
+			InitializeComponent ();
+
+			var threads = new List<ForumThread> ();
+			threads.Add (new ForumThread ("Hello World", 5, "Dylan", "23 Aug 2015"));
+			threads.Add (new ForumThread ("Hello Bob", 9, "Robot", "13 Sep 2015"));
+
+			ForumListView.ItemsSource = threads;
+		}
+	}
+}
+
