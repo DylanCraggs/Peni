12e19484bbcd4836668b589106fc214a34fbf56b
Dylan Craggs <dylancraggs@Dylans-iMac.local>
2015-09-11 15:53:59 +1000

    Forums & Threads ui pretty much done, just a few positioning and design issues that need to be addressed later.

--- Peni/Droid/Resources/Resource.designer.cs
+++ Peni/Droid/Resources/Resource.designer.cs
@@ -26,6 +26,50 @@ namespace Peni.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::Peni.Droid.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::Peni.Droid.Resource.String.Hello;
+			global::XLabs.Platform.Resource.String.ApplicationName = global::Peni.Droid.Resource.String.ApplicationName;
+			global::XLabs.Platform.Resource.String.Hello = global::Peni.Droid.Resource.String.Hello;
+			global::XLabs.Forms.Resource.Color.calendar_active_month_bg = global::Peni.Droid.Resource.Color.calendar_active_month_bg;
+			global::XLabs.Forms.Resource.Color.calendar_bg = global::Peni.Droid.Resource.Color.calendar_bg;
+			global::XLabs.Forms.Resource.Color.calendar_divider = global::Peni.Droid.Resource.Color.calendar_divider;
+			global::XLabs.Forms.Resource.Color.calendar_highlighted_day_bg = global::Peni.Droid.Resource.Color.calendar_highlighted_day_bg;
+			global::XLabs.Forms.Resource.Color.calendar_inactive_month_bg = global::Peni.Droid.Resource.Color.calendar_inactive_month_bg;
+			global::XLabs.Forms.Resource.Color.calendar_selected_day_bg = global::Peni.Droid.Resource.Color.calendar_selected_day_bg;
+			global::XLabs.Forms.Resource.Color.calendar_selected_range_bg = global::Peni.Droid.Resource.Color.calendar_selected_range_bg;
+			global::XLabs.Forms.Resource.Color.calendar_text_active = global::Peni.Droid.Resource.Color.calendar_text_active;
+			global::XLabs.Forms.Resource.Color.calendar_text_inactive = global::Peni.Droid.Resource.Color.calendar_text_inactive;
+			global::XLabs.Forms.Resource.Color.calendar_text_selected = global::Peni.Droid.Resource.Color.calendar_text_selected;
+			global::XLabs.Forms.Resource.Color.calendar_text_selector = global::Peni.Droid.Resource.Color.calendar_text_selector;
+			global::XLabs.Forms.Resource.Color.calendar_text_unselectable = global::Peni.Droid.Resource.Color.calendar_text_unselectable;
+			global::XLabs.Forms.Resource.Dimension.calendar_day_headers_paddingbottom = global::Peni.Droid.Resource.Dimension.calendar_day_headers_paddingbottom;
+			global::XLabs.Forms.Resource.Dimension.calendar_month_title_bottommargin = global::Peni.Droid.Resource.Dimension.calendar_month_title_bottommargin;
+			global::XLabs.Forms.Resource.Dimension.calendar_month_topmargin = global::Peni.Droid.Resource.Dimension.calendar_month_topmargin;
+			global::XLabs.Forms.Resource.Dimension.calendar_text_medium = global::Peni.Droid.Resource.Dimension.calendar_text_medium;
+			global::XLabs.Forms.Resource.Dimension.calendar_text_small = global::Peni.Droid.Resource.Dimension.calendar_text_small;
+			global::XLabs.Forms.Resource.Drawable.ad16 = global::Peni.Droid.Resource.Drawable.ad16;
+			global::XLabs.Forms.Resource.Drawable.calendar_bg_selector = global::Peni.Droid.Resource.Drawable.calendar_bg_selector;
+			global::XLabs.Forms.Resource.Id.calendar_grid = global::Peni.Droid.Resource.Id.calendar_grid;
+			global::XLabs.Forms.Resource.Id.calendar_view = global::Peni.Droid.Resource.Id.calendar_view;
+			global::XLabs.Forms.Resource.Id.left_arrow = global::Peni.Droid.Resource.Id.left_arrow;
+			global::XLabs.Forms.Resource.Id.right_arrow = global::Peni.Droid.Resource.Id.right_arrow;
+			global::XLabs.Forms.Resource.Id.title = global::Peni.Droid.Resource.Id.title;
+			global::XLabs.Forms.Resource.Layout.calendar_pager_layout = global::Peni.Droid.Resource.Layout.calendar_pager_layout;
+			global::XLabs.Forms.Resource.Layout.calendar_picker = global::Peni.Droid.Resource.Layout.calendar_picker;
+			global::XLabs.Forms.Resource.Layout.dialog = global::Peni.Droid.Resource.Layout.dialog;
+			global::XLabs.Forms.Resource.Layout.month = global::Peni.Droid.Resource.Layout.month;
+			global::XLabs.Forms.Resource.Layout.week = global::Peni.Droid.Resource.Layout.week;
+			global::XLabs.Forms.Resource.String.ApplicationName = global::Peni.Droid.Resource.String.ApplicationName;
+			global::XLabs.Forms.Resource.String.Hello = global::Peni.Droid.Resource.String.Hello;
+			global::XLabs.Forms.Resource.String.day_name_format = global::Peni.Droid.Resource.String.day_name_format;
+			global::XLabs.Forms.Resource.String.full_date_format = global::Peni.Droid.Resource.String.full_date_format;
+			global::XLabs.Forms.Resource.String.invalid_date = global::Peni.Droid.Resource.String.invalid_date;
+			global::XLabs.Forms.Resource.String.library_name = global::Peni.Droid.Resource.String.library_name;
+			global::XLabs.Forms.Resource.String.month_name_format = global::Peni.Droid.Resource.String.month_name_format;
+			global::XLabs.Forms.Resource.Style.CalendarCell = global::Peni.Droid.Resource.Style.CalendarCell;
+			global::XLabs.Forms.Resource.Style.CalendarCell_CalendarDate = global::Peni.Droid.Resource.Style.CalendarCell_CalendarDate;
+			global::XLabs.Forms.Resource.Style.CalendarCell_DayHeader = global::Peni.Droid.Resource.Style.CalendarCell_DayHeader;
+			global::XLabs.Forms.Resource.Style.CalendarTitle = global::Peni.Droid.Resource.Style.CalendarTitle;
 		}
 		
 		public partial class Attribute
@@ -41,14 +85,103 @@ namespace Peni.Droid
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int calendar_active_month_bg = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int calendar_bg = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int calendar_divider = 2130968578;
+			
+			// aapt resource value: 0x7f040005
+			public const int calendar_highlighted_day_bg = 2130968581;
+			
+			// aapt resource value: 0x7f040003
+			public const int calendar_inactive_month_bg = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int calendar_selected_day_bg = 2130968580;
+			
+			// aapt resource value: 0x7f040006
+			public const int calendar_selected_range_bg = 2130968582;
+			
+			// aapt resource value: 0x7f040008
+			public const int calendar_text_active = 2130968584;
+			
+			// aapt resource value: 0x7f040007
+			public const int calendar_text_inactive = 2130968583;
+			
+			// aapt resource value: 0x7f040009
+			public const int calendar_text_selected = 2130968585;
+			
+			// aapt resource value: 0x7f04000b
+			public const int calendar_text_selector = 2130968587;
+			
+			// aapt resource value: 0x7f04000a
+			public const int calendar_text_unselectable = 2130968586;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int calendar_day_headers_paddingbottom = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int calendar_month_title_bottommargin = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int calendar_month_topmargin = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int calendar_text_medium = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int calendar_text_small = 2131034116;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int ad16 = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int ThreadCommentIcon = 2130837505;
+			public const int calendar_bg_selector = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int favorite = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int notFavorite = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int ThreadCommentIcon = 2130837509;
 			
 			static Drawable()
 			{
@@ -56,6 +189,121 @@ namespace Peni.Droid
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f080004
+			public const int calendar_grid = 2131230724;
+			
+			// aapt resource value: 0x7f080001
+			public const int calendar_view = 2131230721;
+			
+			// aapt resource value: 0x7f080003
+			public const int left_arrow = 2131230723;
+			
+			// aapt resource value: 0x7f080002
+			public const int right_arrow = 2131230722;
+			
+			// aapt resource value: 0x7f080000
+			public const int title = 2131230720;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int calendar_pager_layout = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int calendar_picker = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int dialog = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int month = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int week = 2130903044;
+			
+			static Layout()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Layout()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f060006
+			public const int ApplicationName = 2131099654;
+			
+			// aapt resource value: 0x7f060005
+			public const int Hello = 2131099653;
+			
+			// aapt resource value: 0x7f060001
+			public const int day_name_format = 2131099649;
+			
+			// aapt resource value: 0x7f060004
+			public const int full_date_format = 2131099652;
+			
+			// aapt resource value: 0x7f060002
+			public const int invalid_date = 2131099650;
+			
+			// aapt resource value: 0x7f060000
+			public const int library_name = 2131099648;
+			
+			// aapt resource value: 0x7f060003
+			public const int month_name_format = 2131099651;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f070001
+			public const int CalendarCell = 2131165185;
+			
+			// aapt resource value: 0x7f070003
+			public const int CalendarCell_CalendarDate = 2131165187;
+			
+			// aapt resource value: 0x7f070002
+			public const int CalendarCell_DayHeader = 2131165186;
+			
+			// aapt resource value: 0x7f070000
+			public const int CalendarTitle = 2131165184;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
