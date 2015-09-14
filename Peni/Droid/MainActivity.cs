d70b50eb2ea7eb689559e1afc91288c1960f32cf
Leandro Rodrigues <leandrovelosorodrigues@gmail.com>
2015-09-15 04:03:27 +1000

    Creating and adapting android lollipop colors and theme

--- Peni/Droid/MainActivity.cs
+++ Peni/Droid/MainActivity.cs
@@ -8,10 +8,12 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Xamarin.Forms.Platform.Android;
 namespace Peni.Droid
 {
 	[Activity (Label = "Peni.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    /*
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -21,6 +23,18 @@ namespace Peni.Droid
 
 			LoadApplication (new App ());
 		}
-	}
+	}*/
+
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
 }
 
