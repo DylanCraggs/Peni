b266eff9613b098ee5ffa6b54edf415127035805
Leandro Rodrigues <leandrovelosorodrigues@gmail.com>
2015-09-15 04:07:33 +1000

    Adding Phelia's MasterDetail Menu Page. Adapting and Integrating Phelia's code. Adding app's icon

--- Peni/Peni/MenuListData.cs
+++ Peni/Peni/MenuListData.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using Xamarin.Forms;
+using System.Collections.Generic;
 
 namespace Peni
 {
@@ -9,28 +10,37 @@ namespace Peni
 		public MenuListData ()
 		{
 			this.Add (new MenuItem () { 
-				Title = "Contracts", 
-				IconSource = "contacts.png", 
-				TargetType = typeof(ContractsPage)
+				Title = "Home", 
+				IconSource = "ic_home_white_48dp.png", 
+				TargetType = typeof(PeniMainContet)
 			});
 
 			this.Add (new MenuItem () { 
-				Title = "Leads", 
-				IconSource = "leads.png", 
-				TargetType = typeof(LeadsPage)
+				Title = "Health", 
+				IconSource = "ic_favorite_white_48dp.png", 
+				TargetType = typeof(DummyPage)
 			});
 
 			this.Add (new MenuItem () { 
-				Title = "Accounts", 
-				IconSource = "accounts.png", 
-				TargetType = typeof(AccountsPage)
+				Title = "Forum", 
+				IconSource = "ic_question_answer_white_48dp.png", 
+				TargetType = typeof(DummyPage)
 			});
 
-			this.Add (new MenuItem () {
-				Title = "Opportunities",
-				IconSource = "opportunities.png",
-				TargetType = typeof(OpportunitiesPage)
-			});
-		}
+            this.Add(new MenuItem()
+            {
+                Title = "Resources",
+                IconSource = "ic_local_library_white_48dp.png",
+                TargetType = typeof(DummyPage)
+            });
+
+            this.Add(new MenuItem()
+            {
+                Title = "Logout",
+                IconSource = "ic_power_settings_new_white_48dp.png",
+                TargetType = typeof(DummyPage)
+            });
+
+        }
 	}
 }
