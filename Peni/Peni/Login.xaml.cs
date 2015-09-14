ef51ad5f802f28c4ba8a4c3cbb4fa5ed4639659a
Sarah Tasker <s.n.tasker@gmail.com>
2015-09-15 02:43:22 +1000

    Whoops finally realised why my pushes weren't working!!!

--- Peni/Peni/Login.xaml.cs
+++ Peni/Peni/Login.xaml.cs
@@ -11,6 +11,24 @@ namespace Peni
 		{
 			InitializeComponent ();
 		}
+			
+
+		protected void ButtonClicked(object sender, EventArgs e)
+		{
+			// These conditions need to be changed once the database is setup
+			if (entryUsername.Text != null) {
+				if (entryPassword.Text != null) {
+					Navigation.PushAsync (new PeniMain ());
+				}
+				else {
+					xLabel.Text = "Please Enter Username and / or Password Again";
+				}
+			} else {
+				xLabel.Text = "Please Enter Username and / or Password Again";
+			}
+		}
 	}
+
+
 }
 
