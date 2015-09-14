cee1024298e6c1a58405464f2b2b81b7713c8b06
Phelia <helloyuetyiu@gmail.com>
2015-09-14 20:10:55 +1000

    Implementing side menu. First Draft. Colors, Links and Icons OK

---
+++ MasterDetail/MasterDetail/MenuListData.cs
@@ -0,0 +1,46 @@
+using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace MasterDetail
+{
+
+	public class MenuListData : List<MenuItem>
+	{
+		public MenuListData ()
+		{
+			this.Add (new MenuItem () { 
+				Title = "Home", 
+				IconSource = "ic_home_white_48dp.png", 
+				TargetType = typeof(DummyPage)
+			});
+
+			this.Add (new MenuItem () { 
+				Title = "Health", 
+				IconSource = "ic_favorite_white_48dp.png", 
+				TargetType = typeof(DummyPage)
+			});
+
+			this.Add (new MenuItem () { 
+				Title = "Forum", 
+				IconSource = "ic_question_answer_white_48dp.png", 
+				TargetType = typeof(DummyPage)
+			});
+
+            this.Add(new MenuItem()
+            {
+                Title = "Resources",
+                IconSource = "ic_local_library_white_48dp.png",
+                TargetType = typeof(DummyPage)
+            });
+
+            this.Add(new MenuItem()
+            {
+                Title = "Logout",
+                IconSource = "ic_power_settings_new_white_48dp.png",
+                TargetType = typeof(DummyPage)
+            });
+
+        }
+	}
+}
---
+++ MasterDetail/MasterDetail/MenuPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace MasterDetail
+{
+	public class MenuPage : ContentPage
+	{
+		public ListView Menu { get; set; }
+
+		public MenuPage ()
+		{
+			Icon = "settings.png";
+			Title = "menu"; // The Title property must be set.
+			BackgroundColor = Color.FromHex ("002351");
+
+			Menu = new MenuListView ();
+
+			var menuLabel = new ContentView {
+				Padding = new Thickness (10, 36, 0, 5),
+				Content = new Label {
+					TextColor = Color.FromHex ("FFFFFF"),
+					Text = "Young Women's Wellness App", 
+				}
+			};
+
+            var embeddedImage = new Image { Aspect = Aspect.AspectFit };
+            embeddedImage.Source = ImageSource.FromResource("circle_flower.png");
+
+            var layout = new StackLayout { 
+				Spacing = 0, 
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+            layout.Children.Add(menuLabel);
+            layout.Children.Add(embeddedImage);
+
+            layout.Children.Add (Menu);
+
+			Content = layout;
+		}
+	}
+}
