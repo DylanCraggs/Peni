5c461fab129b425abdc165d107f865ab487bfb81
Sarah Tasker <s.n.tasker@gmail.com>
2015-09-15 10:55:31 +1000

    Fixed stuff

--- Peni/Peni/Logins.xaml.cs
+++ Peni/Peni/Logins.xaml.cs
@@ -13,20 +13,20 @@ namespace Peni
 		}
 			
 
-		protected void ButtonClicked(object sender, EventArgs e)
-		{
+	//	protected void ButtonClicked(object sender, EventArgs e)
+	//	{
 			// These conditions need to be changed once the database is setup
-			if (entryUsername.Text != null) {
-				if (entryPassword.Text != null) {
-					Navigation.PushAsync (new PeniMain ());
-				}
-				else {
-					xLabel.Text = "Please Enter Username and / or Password Again";
-				}
-			} else {
-				xLabel.Text = "Please Enter Username and / or Password Again";
-			}
-		}
+		//	if (entryUsername.Text != null) {
+		//		if (entryPassword.Text != null) {
+		//			Navigation.PushAsync (new PeniMain ());
+		//		}
+		//		else {
+		//			xLabel.Text = "Please Enter Username and / or Password Again";
+		//		}
+		//	} else {
+		//		xLabel.Text = "Please Enter Username and / or Password Again";
+		//	}
+	//	}
 	}
 
 
