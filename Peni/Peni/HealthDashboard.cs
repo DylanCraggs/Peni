cc168c0b57e29d90c8c97344ae8091f3704bf844
Michaela Dray <mpdray@hotmail.com>
2015-10-02 23:23:14 +1000

    HeathDashboard skeleton added

---
+++ Peni/Peni/HealthDashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Peni
+{
+	public class HealthDashboard : ContentPage
+	{
+		public HealthDashboard ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
