6d2f4968b8ed85d25ce8215052de8c0a97e7bf7c
Sarah Tasker <s.n.tasker@gmail.com>
2015-10-03 18:55:55 +1000

    no message

--- Peni/Peni.Data/Databases/ProfileDatabase.cs
+++ Peni/Peni.Data/Databases/ProfileDatabase.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using SQLite.Net;
+using Xamarin.Forms;
+using System.Linq;
+using System.Collections.Generic;
+using GalaSoft.MvvmLight.Ioc;
 
 namespace Peni.Data
 {
-	public class EmptyClass
+	public class ProfileDatabase
 	{
-		public EmptyClass ()
-		{
-		}
+			SQLiteConnection Connection;
+
+			public ProfileDatabase () {
+				Connection = DependencyService.Get<ISQLite> ().GetConnection ();
+
+				// Check if Thread table exists, if not create it
+				if (Connection.TableMappings.All(t => t.MappedType.Name != typeof(UserProfile).Name)) {
+					Connection.CreateTable<Thread> ();
+					Connection.Commit ();
+				}
+			}
+
 	}
 }
 
--- Peni/Peni.Data/Models/Profile/UserProfile.cs
+++ Peni/Peni.Data/Models/Profile/UserProfile.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using SQLite.Net.Attributes;
 
-namespace Peni.Data
+namespace Peni
 {
-	public class EmptyClass
+	public class UserProfile
 	{
-		public EmptyClass ()
+		[PrimaryKey, AutoIncrement]
+		public int id { get; set; }
+		public string LocalUsername { get; set; }
+		public int UserStage { get; set; }
+		public string UserBio { get; set; }
+		public string UserStatus { get; set; }
+		public bool UserPrivacy { get; set; } 
+
+		public UserProfile() {}
+
+		public UserProfile (string LocalUsername, int UserStage,
+			string UserBio)
 		{
+			this.LocalUsername = LocalUsername;
+			this.UserStage = UserStage;
+			this.UserBio = UserBio;
+			this.UserStatus = UserStatus;
+			this.UserPrivacy = UserPrivacy;
+
 		}
+
 	}
 }
 
