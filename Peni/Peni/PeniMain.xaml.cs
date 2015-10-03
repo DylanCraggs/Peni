55ea1ccb175b4234e22346a9e1ced9b3de8d9771
Leandro Rodrigues <leandrovelosorodrigues@gmail.com>
2015-10-03 23:16:19 +1000

    Changing home screen layout. adding notification imgs.

--- Peni/Peni/PeniMain.xaml.cs
+++ Peni/Peni/PeniMain.xaml.cs
@@ -7,48 +7,97 @@ using Peni.Data.ViewModel;
 namespace Peni
 {
 
-	public class PeniMainContet : ContentPage
+	public partial class PeniMainContet : ContentPage
 	{
 		public PeniMainContet()
 		{
-			Title = "Home";
-			Icon = "drawable/ic_home_white_48dp.png";
+			InitializeComponent();
 
-			Image image = new Image
+			Grid myGrid = new Grid();
+			myGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+			myGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+			Image peni = new Image
 			{
-				// Some differences with loading images in initial release.
-				Source = ImageSource.FromFile("peni_small.png"),
-				HeightRequest = 300,
-				VerticalOptions = LayoutOptions.CenterAndExpand
+				
+				HeightRequest = 400,
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center,
+				BackgroundColor = Color.White
+			};
+			//Logic to change peni expression (img). Must be finished after implementing health database and logic
+			//1 == Defaul expression (Happy)/ 2 == thirsty
+			int expression = 2;
+
+			switch (expression) {
+			case 1:
+				peni.Source = "peni_small.png";
+				break;
+			case 2:
+				peni.Source = "peni_thirsty.png";
+				break;
+			default:
+				peni.Source = "peni_small.png";
+				break;
+			}
+
+			myGrid.Children.Add (peni);
+			Grid.SetRow (peni, 1);
+			Grid.SetColumn (peni, 0);
+			Grid.SetColumnSpan (peni, 3);
+
+			//If there's water notification, then display image. Must be finished the logic with the database
+			if (true) {
+				Image waterNotification = new Image {
+					Source = ImageSource.FromFile ("water_cloud.png"),
+					HeightRequest = 100,
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					BackgroundColor = Color.White
+				};
+
+				myGrid.Children.Add (waterNotification);
+				Grid.SetRow (waterNotification, 0);
+				Grid.SetColumn (waterNotification, 0);
+			}
+
+			//If there's food notification, then display image. Must be finished the logic with the database
+			if (true) {
+				Image foodNotification = new Image {
+					Source = ImageSource.FromFile ("food_cloud.png"),
+					HeightRequest = 100,
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					BackgroundColor = Color.White
+				};
+
+
+				myGrid.Children.Add (foodNotification);
+				Grid.SetRow (foodNotification, 0);
+				Grid.SetColumn (foodNotification, 2);
+			}
+
+			//If there's other notification, then display image. Must be finished the logic with the database
+			if (true) {
+				Image otherNotification = new Image {
+					Source = ImageSource.FromFile ("some_cloud.png"),
+					HeightRequest = 100,
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					BackgroundColor = Color.White
+				};
+
+				myGrid.Children.Add (otherNotification);
+				Grid.SetRow (otherNotification, 0);
+				Grid.SetColumn (otherNotification, 1);
+			}
+
+
+			Content = new Frame { 
+				Content = myGrid
 			};
 
-			StackLayout stacklayout = new StackLayout {
-
-				BackgroundColor = Color.White,
-				Padding = new Thickness(20), // change to be universal to rest of app. 
-
-				Children = {
-
-					image,
-
-					new Label { Text = "this is the home page!!!!!!!!!!!" }
-
-				} // ends children
-
-
-			}; // ends stacklayout
-
-			Content = new ScrollView { 
-
-				// call all the content for the scroll view here
-
-				Content = stacklayout
-
-			};
-
-
-		} // ends pibli
-
+		}
 	}// ends public class
 
 	public class PeniMasterDetail : MasterDetailPage
